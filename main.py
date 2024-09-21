42e53ad71f7fdc4b1efe7812dd600fdbae43ee4f
jakka <jakkadoujin@gmail.com>
2024-09-21 11:44:28 +0300

    quick fix

--- main.py
+++ main.py
@@ -2,51 +2,9 @@
 
 import sys, requests, re, json, time
 
-key = "api-key"
+key = "ZeUGE0md3G8ohnUr03zR3Wixd5S9owf6gzerjp1F5gA="
 server_url = 'https://dev-api.jakka.su/'
 
-
-def query_job(jobid):
-    time.sleep(1)
-    response = requests.get(server_url + "job", headers={'api-key': key}, json={'jobid': str(jobid)})
-    msg = response.json()['message']
-    while msg != 'completed':
-        if msg == "retry" or msg == "archived":
-            sys.stdout.write("Error: " + response.json()['message'])
-            sys.stdout.flush()
-            exit(1)
-        elif msg == "active":
-            sys.stdout.write("Processing")
-            sys.stdout.flush()
-            for _ in range(3):
-                sys.stdout.write(".")
-                sys.stdout.flush()
-                time.sleep(0.6)
-            sys.stdout.write("\n")
-            sys.stdout.flush()
-            print ("\033[A                             \033[A")
-            response = requests.get(server_url + "job", headers={'api-key': key}, json={'jobid': str(jobid)})
-            msg = response.json()['message']
-        elif msg == "aggregating" or msg == "scheduled" or msg == "pending":
-            sys.stdout.write("In queue")
-            sys.stdout.flush()
-            for _ in range(3):
-                sys.stdout.write(".")
-                sys.stdout.flush()
-                time.sleep(0.6)
-            sys.stdout.write("\n")
-            sys.stdout.flush()
-            print ("\033[A                             \033[A")
-            response = requests.get(server_url + "job", headers={'api-key': key}, json={'jobid': str(jobid)})
-            msg = response.json()['message']
-        else:
-            if response.status_code == 200:
-                break
-            else:
-                sys.stdout.write("Error: " + response.json()['message'])
-                sys.stdout.flush()
-                exit(1)
-        return response.json()['message']
 
 
 if __name__ == '__main__':
@@ -57,10 +15,54 @@ if __name__ == '__main__':
             print(response.json()['message'])
         else:
             jobid = response.json()['message']
-            server_filename = query_job(jobid)
+            time.sleep(0.5)
+            response = requests.get(server_url + "job", headers={'api-key': key}, json={'jobid': str(jobid)})
+            msg = response.json()['message']
+            print()
+            while msg != 'completed':
+                if msg == "retry" or msg == "archived":
+                    sys.stdout.write("Error: " + response.json()['message'])
+                    sys.stdout.flush()
+                    exit(1)
+                elif msg == "active":
+                    print ("\033[A                             \033[A")
+                    sys.stdout.write("Processing")
+                    sys.stdout.flush()
+                    time.sleep(0.5)
+                    for _ in range(3):
+                        sys.stdout.write(".")
+                        sys.stdout.flush()
+                        time.sleep(0.5)
+                    sys.stdout.write("\n")
+                    sys.stdout.flush()
+                    response = requests.get(server_url + "job", headers={'api-key': key}, json={'jobid': str(jobid)})
+                    msg = response.json()['message']
+                elif msg == "aggregating" or msg == "scheduled" or msg == "pending":
+                    print ("\033[A                             \033[A")
+                    sys.stdout.write("In queue")
+                    sys.stdout.flush()
+                    time.sleep(0.5)
+                    for _ in range(3):
+                        sys.stdout.write(".")
+                        sys.stdout.flush()
+                        time.sleep(0.5)
+                    sys.stdout.write("\n")
+                    sys.stdout.flush()
+                    response = requests.get(server_url + "job", headers={'api-key': key}, json={'jobid': str(jobid)})
+                    msg = response.json()['message']
+                else:
+                    if response.status_code == 200:
+                        break
+                    else:
+                        sys.stdout.write("Error: " + response.json()['message'])
+                        sys.stdout.flush()
+                        exit(1)
+
+            server_filename = response.json()["message"]
             filename = server_filename.split('/')[-1]
 
             response = requests.get(server_url + "file", headers={'api-key': key}, json={'url': server_filename})
+            print ("\033[A                             \033[A")
             if response.headers.get('Content-Type') == 'application/zip':
                 with open(filename, 'wb') as f:
                     print("Downloading ", filename.split('.zip')[0])
@@ -72,6 +74,7 @@ if __name__ == '__main__':
                         done = int(50 * dl / total_length)
                         sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )
                         sys.stdout.flush()
+                print()
             else:
                 print(response.json()['message'])
     else:
