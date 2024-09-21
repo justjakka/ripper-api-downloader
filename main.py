03f9fa9d91006b6bf5588b8796d66a812f7b7a68
jakka <jakkadoujin@gmail.com>
2024-09-21 12:07:20 +0300

    added script with unarchiving and m4a convertion

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python
 
-import sys, requests, json, time
+import sys, requests, json, time, subprocess, os
+
+from mutagen.mp4 import MP4
+from mutagen.flac import FLAC
+from subprocess import call
 
 key = "api-key"
 server_url = 'https://api.jakka.su/'
 
+def convert_m4a():
+    m4as = []
+    metadata = {}
+    for subdir, dirs, files in os.walk('./'):
+        for file in files:
+            if file.endswith(".m4a"):
+                m4as.append(file)
+                flac = file[:-4] + ".flac"
+                metadata[file] = flac
+                call(["ffmpeg", "-hide_banner", "-loglevel", "error", "-i", file, flac])
+                call(["flac", "-8", "-f", flac])
 
+
+    for m4a in m4as:
+        m4a_a = MP4(m4a)
+        flac_a = FLAC(metadata[m4a])
+        flac_a.delete()
+        flac_a["TITLE"] = m4a_a["\xa9nam"]
+        flac_a["ALBUM"] = m4a_a["\xa9alb"]
+        flac_a["ARTIST"] = m4a_a["\xa9ART"]
+        flac_a["ALBUMARTIST"] = m4a_a["aART"]
+        flac_a["DATE"] = '.'.join(m4a_a["\xa9day"][0].split('-'))
+        try:
+            flac_a["DISCNUMBER"] = str(m4a_a["disk"][0][0])
+        except Exception:
+            pass
+        try:
+            if str(m4a_a["disk"][0][1]) == '' or str(m4a_a["disk"][0][1]) == '0':
+                flac_a["DISCTOTAL"] = "1"
+            else:
+                flac_a["DISCTOTAL"] = str(m4a_a["disk"][0][1])
+        except Exception:
+            pass
+        flac_a["TRACKNUMBER"] = str(m4a_a["trkn"][0][0])
+        flac_a["TRACKTOTAL"] = str(m4a_a["trkn"][0][1])
+        flac_a.save()
+        os.remove(m4a)
 
 if __name__ == '__main__':
     response = requests.post(server_url, headers={'api-key': key}, json={'url': sys.argv[1]})
@@ -75,6 +115,11 @@ if __name__ == '__main__':
                         sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )
                         sys.stdout.flush()
                 print()
+
+                subprocess.call(['7z', 'x', '-o' + filename.split('.zip')[0], filename])
+                os.remove(filename)
+                os.chdir("./" + filename.split('.zip')[0])
+                convert_m4a()
             else:
                 print(response.json()['message'])
     else:
